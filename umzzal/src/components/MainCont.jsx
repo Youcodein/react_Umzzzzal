e1fcf1773d2a64c90c33c2c51dd89faf7df85a0d
HWANGYoujin <ghkddbwls96@gmail.com>
2022-12-06 07:36:33 +0900

    asdf

--- umzzal/src/components/MainCont.jsx
+++ umzzal/src/components/MainCont.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchAPI } from '../utils/fetchAPI'
+import { Category } from './'
 
-const MainCont = () => {
+
+
+const MainConts = () => {
+  // const [selectCategory, setSelectCategory] = useState('소품샵')
+  // const [videos, setVideos] = useState(null)
+
+  // useEffect(() => {
+  //   fetchAPI(`search?part=snippet&q=${selectCategory}&type=video`).then((data) =>
+  //   setVideos(data.items)
+  //   )
+  // }, [selectCategory])
+
   return (
-    <div>MainCont</div>
+    <main id="main">
+      <aside id="aside">
+        <Category
+          selectCategory={selectCategory}
+          setSelectCategory={setSelectCategory}
+        />
+      </aside>
+      <section id="contents">
+        <h2>
+          <em>{selectCategory}</em> 유튜브
+        </h2>
+        <Videos 
+        videos={videos}
+        setVideos = {setVideos}
+        />
+      </section>
+    </main>
   )
 }
 
-export default MainCont
+export default MainConts
